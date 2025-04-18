62bdd09819d14e274e0001d306bbca81c7315ad1
Sérgio Almeida <almeida.sergiomc@gmail.com>
2025-04-18 15:20:32 +0100

    added logic for drink search by ingredient, showing drink title, image and instructions

--- js/main.js
+++ js/main.js
@@ -1,27 +1,77 @@
-//Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch() {
-  // clean the caroussel by deleting all divs
+// function to clean the gallery by deleting all divs
+function deleteDivs() {
   document.querySelectorAll('div').forEach(div => div.remove())
+}
+
+function getFetch() {
+  // clean the gallery
+  deleteDivs()
   // set ingredientChosen to what's in the input
   let ingredientChosen = document.querySelector("input").value
   // API url
-  const url = `www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientChosen}`
+  const drinksByIngredientURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientChosen}`
 
-  fetch(url)
+  fetch(drinksByIngredientURL)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        /* EXAMPLE FOR CREATING AN ELEMENT: 
-        for (let i = 0; i < data.photos.length; i++) {
-          let newImg = document.createElement("img")
-          newImg.src = data.photos[i].img_src
-          document.querySelector("span").insertAdjacentElement("afterend", newImg)
+        
+        // if no drinks are found
+        if (data.drinks === "no data found") {
+          document.querySelector("#possibleDrinks").innerText = "0 drinks found, try again!"
+          return
+        } else {
+          document.querySelector("#possibleDrinks").innerText = "Possible drinks with this ingredient:"
         }
-        */
+
+        // for each drink found
+        for (let i = 0; i < data.drinks.length; i++) {
+
+          // create a new div to later add as the last child of the section drinkCards
+          let newCard = document.createElement("div")
+          
+          // add the drink title
+          let newTitle = document.createElement("p")
+          newTitle.innerText = data.drinks[i].strDrink
+          newCard.insertAdjacentElement("beforeend", newTitle)
+          
+          // add the drink image
+          let newImg = document.createElement("img")
+          newImg.src = data.drinks[i].strDrinkThumb
+          newCard.insertAdjacentElement("beforeend", newImg)
+
+          let currentDrinkID = data.drinks[i].idDrink
+          // now 
+          const drinkURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${currentDrinkID}`
+
+          fetch(drinkURL)
+              .then(res => res.json()) // parse response as JSON
+              .then(data => {
+                console.log(data)
+                
+                // add the drink instructions
+                let drinkInstruction = document.createElement("p")
+                drinkInstruction.innerText = data.drinks[0].strInstructions
+                newCard.insertAdjacentElement("beforeend", drinkInstruction)
+        
+                }
+              )
+              .catch(err => {
+                  console.log("something went wrong with the drink ID")
+                  console.log(`error ${err}`)
+              });
+          
+          // at the end, add the card to the section drinkCards
+          document.querySelector("#drinkCards").insertAdjacentElement("beforeend", newCard)         
+        }
+
       })
       .catch(err => {
+          // if the ingredient typed is invalid
+          deleteDivs()
+          document.querySelector("#possibleDrinks").innerText = "0 drinks found, try again!"
           console.log(`error ${err}`)
       });
 }
